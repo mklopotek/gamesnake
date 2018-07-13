6241d314b682a61b483c2f22961103a5c852ac7e
Magda Kłopotek <magda.klopotek@gmail.com>
2018-07-13 16:15:14 +0200

    prototype added

--- snake.js
+++ snake.js
@@ -2,203 +2,159 @@
 // (function () {
 
 //"GLOBAL" VARIABLES
-    
-    //this two variable are the number of rows (areaLengthX) and the number of columns (areaLenghtY)
-    var areaRowsLengthX = 10
-    var areaColumnsLengthY = 10
+function GameSnake() {
+    this.areaRowsLengthX = 10
+    this.areaColumnsLengthY = 10
 
-    var gameContainer = ''
-    var scoreContainer = ''
-    // var timeContainer
+    this.gameContainer = ''
+    this.scoreContainer = ''
 
     //here we declaared that when we call initialArea it call the function createEmptyArea and give her a two variable -areaRowsLenghtX and areaColumnsLenghtY 
-    var initialArea = createEmptyArea(areaRowsLengthX, areaColumnsLengthY)
-
-    // console.log(initialArea) //it works
+    this.initialArea = this.createEmptyArea(this.areaRowsLengthX, this.areaColumnsLengthY)
 
     // var clearArea = JSON.parse(JSON.stringify(initialArea))  //i dont know yet if there should be a clear, first area or only one area for all
-    var area = JSON.parse(JSON.stringify(initialArea))
-
-    // console.log(clearArea)                   //it works
+    this.area = JSON.parse(JSON.stringify(this.initialArea))
     // console.log(initialArea !== clearArea)   //it works because it says: true! (so it copy a old area but without references!!!! (changing area not changing a initialArea))
 
-
-    const initialPosition = (whatRow, whatColumn) => ({
+    this.initialPosition = (whatRow, whatColumn) => ({
         whatRow: whatRow,
         whatColumn: whatColumn
     })
 
-    var initialPositionSnakeHead = initialPosition(6, 6)
-    var initialPositionSnakeBody1 = initialPosition(7, 6)
-    var initialPositionSnakeBody2 = initialPosition(8, 6)
-    var initialPositionFood = initialPosition(3, 3)
+    this.initialPositionSnakeHead = this.initialPosition(6, 6)
+    this.initialPositionSnakeBody1 = this.initialPosition(7, 6)
+    this.initialPositionSnakeBody2 = this.initialPosition(8, 6)
+    this.initialPositionFood = this.initialPosition(3, 3)
+    this.foodPosition = null
+    this.score = null
+    this.gameInterval = null
+    this.container = document.body
 
-    // console.log(initialPositionSnakeHead)
 
-    var foodPosition
+    this.init()
+}
 
-    var time
-    var score 
+GameSnake.prototype.createEmptyArea = function (areaRowsLengthX, areaColumnsLengthY) {
+    return Array(areaRowsLengthX).fill('0').map(function (element, index, array) {
+        return Array(areaColumnsLengthY).fill('0')
+    })
+}
 
-    var gameInterval 
+GameSnake.prototype.init = function () {
+    this.prepareLayout()
+    this.addSnakeAndFoodToArea()
+    this.render()
+}
 
-    //this function create clear area
+GameSnake.prototype.prepareLayout = function () {
 
-    function createEmptyArea(areaRowsLengthX, areaColumnsLengthY){
-        return Array(areaRowsLengthX).fill('0').map(function(element, index, array){
-            return Array(areaColumnsLengthY).fill('0')
-        })
+    function makeGameContainer() {
+        gameContainer = document.createElement('div')
+        gameContainer.classList.add('game')
+
+        return gameContainer
     }
 
-    // console.log(createEmptyArea(10,2))     //it works
+    this.gameContainer = makeGameContainer()
+    this.scoreContainer = document.createElement('div')
+    this.container.appendChild(this.scoreContainer)
+    this.container.appendChild(this.gameContainer)
+}
 
-    function init(container) {
-        prepareLayout(container)
-        addSnakeAndFoodToArea()
-        render()
-        // this function should be called when we want to init game
-        // it accepts 1 argument - dom node of the container
-        // where game should be rendered, eg it can be body of document
-        // this function should render first frame of game and set all
-        // of the variables like time to game end that werent predefinied
-    } 
-    
-    function prepareLayout(container) {
+//this function will be transform the table from js to HTML 
+GameSnake.prototype.render = function () {
+    this.gameContainer.innerHTML = ''
 
-        function makeGameContainer(){
-            var gameContainer = document.createElement('div')
-            gameContainer.classList.add('game')
-    
-            return gameContainer
-        }
+    this.area.forEach((areaRow, index, array) => {
+        var row = this.makeRowsforElementsInArea()
 
-        gameContainer = makeGameContainer()
-
-        scoreContainer = document.createElement('div')
-        // timeContainer = document.createElement('div')
-        container.appendChild(scoreContainer)
-        // container.appendChild(timeContainer)
-        container.appendChild(gameContainer)
-    }
-
-    // console.log(prepareLayout())
-
-    //this function will be transform the table from js to HTML 
-    function render() {
-
-        gameContainer.innerHTML = ''
-
-        // console.log(gameContainer)
-    
-        area.forEach(function (areaRow, index, array) {
-
-            var row = makeRowsforElementsInArea()
-
-            areaRow.forEach(function(element, index, array){
-
-                var cell = makeCellforElementsThatInElementsInArea(element)
-
-                // console.log(cell)
-
-                row.appendChild(cell)
-
-                // console.log(row)
-
-            })
-
-            gameContainer.appendChild(row)
-
-            console.log(gameContainer)   //it work i hope ;) 
-
+        areaRow.forEach( (element, index, array) => {
+            var cell = this.makeCellforElementsThatInElementsInArea(element)
+            row.appendChild(cell)
         })
 
+        this.gameContainer.appendChild(row)
+
+        console.log(this.gameContainer)   //it work i hope ;) 
+    })
+}
+
+
+// console.log(render())     
+
+GameSnake.prototype.makeRowsforElementsInArea = function () {
+    const row = document.createElement('div')
+    row.classList.add('game__row')
+    return row
+}
+
+GameSnake.prototype.makeCellforElementsThatInElementsInArea = function (element) {
+    const makeGameElement = (modifier) => () => {
+        const el = document.createElement('div')
+        el.classList.add('game__cell--' + modifier)
+        return el
     }
 
+    const makeCellEmpty = makeGameElement('empty')
+    const makeCellHeadSnake = makeGameElement('snake-head')
+    const makeCellBodySnake = makeGameElement('snake-body')
+    const makeCellFood = makeGameElement('food')
 
-    // console.log(render())     
-
-    //helper for render 
-
-    function makeRowsforElementsInArea(){
-        var row = document.createElement('div')
-        var atribute = document.createAttribute('class')
-        atribute.value = 'game__row'
-        row.setAttributeNode(atribute)
-
-        return row
-
+    // @TODO - change if else to switch
+    if (element === '0') {
+        return makeCellEmpty()
+    } else if (element === 'H') {
+        return makeCellHeadSnake()
+    } else if (element === '1') {
+        return makeCellBodySnake()
+    } else if (element === 'F') {
+        return makeCellFood()
     }
+}
 
-    // console.log(makeRowsforElementsInArea())  //it works but maybe I should change the name of class for row? 
+GameSnake.prototype.addSnakeAndFoodToArea = function () {
+    this.area[this.initialPositionSnakeHead.whatRow][this.initialPositionSnakeHead.whatColumn] = 'H'
+    this.area[this.initialPositionSnakeBody1.whatRow][this.initialPositionSnakeBody1.whatColumn] = '1'
+    this.area[this.initialPositionSnakeBody2.whatRow][this.initialPositionSnakeBody2.whatColumn] = '1'
+    this.area[this.initialPositionFood.whatRow][this.initialPositionFood.whatColumn] = 'F'
+}
 
-    function makeCellforElementsThatInElementsInArea(element){
-        const makeGameElement = (modifier) => () => {
-            const el = document.createElement('div')
-            el.classList.add('game__cell--' + modifier)
-            return el
-        }
 
-        const makeCellEmpty = makeGameElement('empty')   
-        const makeCellHeadSnake = makeGameElement('snake-head')
-        const makeCellBodySnake = makeGameElement('snake-body')
-        const makeCellFood = makeGameElement('food')
+// // console.log(area)   //it works the clear area have changed and added H, F, 1, 1 :) 
 
-        // @TODO - change if else to switch
-        if(element === '0'){
-           return  makeCellEmpty() 
-        } else if(element === 'H') {
-           return makeCellHeadSnake()
-        } else if(element === '1'){
-           return makeCellBodySnake()
-        } else if(element === 'F'){
-            return makeCellFood()
-        }
-    }
+// // here you can put some functions taht renders only parts of the game 
+// // and will be used in render function
 
-    function addSnakeAndFoodToArea () {
-        area[initialPositionSnakeHead.whatRow][initialPositionSnakeHead.whatColumn] = 'H'
+// // here you will attach all events listeners like oncliks or keydowns
+// function attachEventListeners() { }
 
-        area[initialPositionSnakeBody1.whatRow][initialPositionSnakeBody1.whatColumn] = '1'
-        area[initialPositionSnakeBody2.whatRow][initialPositionSnakeBody2.whatColumn] = '1'
+// // move should be another function called eg. when event is fired
+// // it is quite obvious that move bakwards is a move fovard with minus sign ;)
+// function move() { }
 
-        area[initialPositionFood.whatRow][initialPositionFood.whatColumn] = 'F'
-    }
+// // in this fucntion you can do all stuff that needs to be repeated
+// // you can invoke this function in an interval
+// // you can set that interval in init function
+// function gameTick() { }
 
- 
-    // console.log(area)   //it works the clear area have changed and added H, F, 1, 1 :) 
+// // below functions are self-describing ;)
+// function incScore() { }
+// function displayScore() { }
 
-    // here you can put some functions taht renders only parts of the game 
-    // and will be used in render function
+// function decTime() { }
+// function displayTIme() { }
 
-    // here you will attach all events listeners like oncliks or keydowns
-    function attachEventListeners() { }
+// // invoked when game ends (you can check if time elepsed eg. in gameTick function)
+// function endGame() { }
 
-    // move should be another function called eg. when event is fired
-    // it is quite obvious that move bakwards is a move fovard with minus sign ;)
-    function move() { }
+// // HELPERS
 
-    // in this fucntion you can do all stuff that needs to be repeated
-    // you can invoke this function in an interval
-    // you can set that interval in init function
-    function gameTick() { }
+// // here put some functions that are not directly itto the game
+// // but will help to do some general stuff - like make an array of ...
 
-    // below functions are self-describing ;)
-    function incScore() { }
-    function displayScore() { }
+// // START GAME
+// // document.body is an example of the container for the game
 
-    function decTime() { }
-    function displayTIme() { }
+// // })()
 
-    // invoked when game ends (you can check if time elepsed eg. in gameTick function)
-    function endGame() { }
-
-    // HELPERS
-
-    // here put some functions that are not directly itto the game
-    // but will help to do some general stuff - like make an array of ...
-
-    // START GAME
-    // document.body is an example of the container for the game
-
-    init(document.body)
-// })()
+new GameSnake()
